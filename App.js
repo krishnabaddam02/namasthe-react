f0c140315b7bb194b795c1d8fac50b14ff0d91c3
krishna <krishnabaddam02>
2023-06-20 21:35:10 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1",{id:'heading'},"Hello world from react")
-const root =  ReactDOM.render(heading, document.getElementById("root"))
+// React.createElement => object ==> html element (render)
+
+//const heading = React.createElement("h1",{},"namasthe krishna")
+
+//jsx (transpiled before it reaches JS) -PARCEL - babel
+
+const Title =()=> (<h1 id="heading" tabIndex ="5">namasthe react</h1>)
+
+const number = 1000
+
+const HeadingComponent = ()=> (
+<div>
+    {number}
+<h1 className="heading">Namasthe react functional Component</h1>
+    </div>)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingComponent/>)
